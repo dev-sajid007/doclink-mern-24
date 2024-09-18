ccea2c96c672c784e839e56fa0dd57fcd832aa86
dev-sajid007 <dev.sajid007@gmail.com>
2024-09-18 22:52:21 +0600

    Doctor Appoinment Page Complete

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,8 +4,12 @@ import {doctors} from "../assets/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = ({children}) => {
+
+  const currencySymbol = "$";
+
   const value = {
-    doctors
+    doctors,
+    currencySymbol
   };
 
   return (
